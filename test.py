5361e05307dcba3a3d7d71f7c63edb7ced745ab6
eugene kingfish <aojg@protonmail.ch>
2023-08-15 09:39:41 +0100

    updated python code

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-from linear_quivers import *
+import pandas as pd
 from time import time
 
-lq = linear_quiver(5,[(2,4)])
-lq.calculate_jectives()
-pr = lq.projective_resolution()
-mat = matrix_of_proj_res(pr)
-print("Matrix of projective resolution:\n\n", mat)
-print(is_fcy(mat, 12))
+from linear_quivers import *
+from relations import *
+
+df = pd.DataFrame()
+for n in range(25):
+    start = time()
+    rels = length_k_relations(n,9)
+
+    for rel in rels:
+        lq = linear_quiver(n, rel)
+        pr = lq.projective_resolution()
+        mat = matrix_of_proj_res(pr)
+        result = is_fcy(mat, 250)
+        new_record = pd.DataFrame([{"n" : n, "Relations" : rel, "FCY Result" : result}])
+        df = pd.concat([df, new_record], ignore_index=True)
+
+    end = time()
+    print(n, end-start)
+print("Compute time taken:", end-start)
+
+print(df)
+df.to_csv("l9_relations.csv")
+
+
+
