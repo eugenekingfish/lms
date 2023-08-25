60a86df67fe9d7b19e4c9c857c3b4c5842adf5b3
eugenekingfish <aojg@protonmail.ch>
2023-08-25 12:05:26 +0100

    added interactive fcy dimension tool

---
+++ fcy_tool.py
@@ -0,0 +1,36 @@
+from linear_quivers import *
+from relations import *
+from time import time, sleep
+
+step_through_mode = True
+
+n = int(input("Enter number of vertices in quiver: "))
+k = int(input("Enter length of relations: "))
+max_iter = int(input("Enter maximum Serre functor applications: "))
+
+start = time()
+rels = length_k_relations(n,k)
+end = time()
+
+print(("-" * 100) + "\n")
+
+print("-> Linear quiver contains", n, "vertices.")
+print("-> Computed", len(rels), "length", k, "relations in", end - start, "seconds.\n")
+
+print(("-" * 100) + "\n")
+
+start = time()
+
+for rel in rels:
+    lq = linear_quiver(n, rel)
+    pr = lq.projective_resolution()
+    print("Relation ->", rel, "\nOutput ->", lq.serre_resolution_fast(max_iter, True))
+    print("\n" + ("-" * 100) + "\n")
+    if step_through_mode:
+        while True:
+            inpt = input("Enter 'y' to proceed to next relation: ")
+            if inpt == 'y':
+                break
+end = time()
+
+print("=> FINISHED COMPUTATION IN", end - start, "SECONDS.\n")
